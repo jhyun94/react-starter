2686283b4766c5505f788db3beeffa9d20d171d8
jhyun94 <hyunjonathan@gmail.com>
2017-08-21 18:35:00 -0500

    wrote first component but does not work yet

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// component that generates html
+const App = function() {
+  return <div>Hi!</div>;
+}
+// take the component and put it on the DOM
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />);
