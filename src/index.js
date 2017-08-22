6637c8cbaee02fae5559dd9f2b31c743d09c8a9a
jhyun94 <hyunjonathan@gmail.com>
2017-08-22 17:54:58 -0500

    created component folder and js file for each components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // component that generates html
-const App = function() {
+const App = () => {
   return <div>Hi!</div>;
 }
 // take the component and put it on the DOM
 
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
