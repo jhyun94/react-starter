1e7a38ae32f916f279bbd1ca67f2a679b187eb33
jhyun94 <hyunjonathan@gmail.com>
2017-08-25 17:56:24 -0500

    able to hardcode search and get data back from youtube

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search-bar';
 
 const API_KEY = 'AIzaSyDWgIFckPKsS74hGznX8HJpTh4cCVRwGGA';
+
+YTSearch({key: API_KEY, term: 'surfboard'}, function(data){
+  console.log(data);
+})
+
 const App = () => {
   return (
       <div>
