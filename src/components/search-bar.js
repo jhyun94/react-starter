6ca9ad427b1fe6970d83ef7e22ee82f173d319d6
jhyun94 <hyunjonathan@gmail.com>
2017-09-04 12:39:57 -0500

    able to get value from input

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -4,8 +4,12 @@ class SearchBar extends React.Component{
 
   render() {
     return (
-      <div><input /></div>
+      <div><input onChange={this.onInputChange} /></div>
     )
+  }
+
+  onInputChange(event){
+    console.log(event.target.value);
   }
 }
 
