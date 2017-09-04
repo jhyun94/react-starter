b5f44bc925454b56875596d4934e96d6cfaff080
jhyun94 <hyunjonathan@gmail.com>
2017-09-04 16:54:26 -0500

    able to display video, title, and description

--- src/components/video-detail.js
+++ src/components/video-detail.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 const VideoDetail = (props) => {
+  const video = props.selectedVideo;
+  if (!video){
+    return <div>loading...</div>
+  }
+  const url = `https://youtube.com/embed/${video.id.videoId}`;
+  console.log(video);
+
   return(
-    <div>working</div>
+    <div className="detail-content col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive" src={url}></iframe>
+      </div>
+      <div className="detail">
+        <h2>{video.snippet.title}</h2>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
   )
 }
 
