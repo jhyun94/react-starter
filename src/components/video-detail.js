cc3046830a9d18a4be9f24c8d81286886fa63337
jhyun94 <hyunjonathan@gmail.com>
2017-09-04 15:12:02 -0500

    able to pass selectedvideo to video-detail component

---
+++ src/components/video-detail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VideoDetail = (props) => {
+  return(
+    <div>working</div>
+  )
+}
+
+export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search-bar';
+import VideoDetail from './components/video-detail';
 
 const YT_key = 'AIzaSyDWgIFckPKsS74hGznX8HJpTh4cCVRwGGA'
 
@@ -10,13 +11,22 @@ class App extends React.Component {
   constructor(props){
     super(props);
 
-  YTSearch({key: YT_key, term: 's2000'}, function(response){
-    console.log(response)
+  this.state = {
+    selectedVideo: null
+  };
+
+  YTSearch({key: YT_key, term: 's2000'}, (videos) => {
+      this.setState({selectedVideo: videos[0]});
     })  
   }
 
   render() {
-    return <SearchBar />
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail selectedVideo={this.state.selectedVideo} />
+      </div>
+    )
   }
 
 }
