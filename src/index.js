157f0fa9dab90d8b299e86863d2ebb3c58f1a251
jhyun94 <hyunjonathan@gmail.com>
2017-09-04 12:57:38 -0500

    able to get response back from youtube api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search-bar';
 
-class App extends React.Component {
+const YT_key = 'AIzaSyDWgIFckPKsS74hGznX8HJpTh4cCVRwGGA'
 
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+  YTSearch({key: YT_key, term: 's2000'}, function(response){
+    console.log(response)
+    })  
+  }
 
   render() {
     return <SearchBar />
