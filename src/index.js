d448f5ac39323db4a2352418c8a5b72b557a3887
jhyun94 <hyunjonathan@gmail.com>
2018-02-10 12:31:59 -0600

    initial

--- src/index.js
+++ src/index.js
@@ -1,53 +1,17 @@
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import _ from 'lodash';
-
-import SearchBar from './components/search-bar';
-import VideoDetail from './components/video-detail';
-import VideoList from './components/video-list';
 
 const YT_key = 'AIzaSyDWgIFckPKsS74hGznX8HJpTh4cCVRwGGA';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
+export default class App extends Component{
 
-  this.state = {
-    selectedVideo: null,
-    videos: []
-  };
-
-  this.SearchYT('s2000')
-}
-
-
-
-
-  SearchYT(term){
-    YTSearch({key: YT_key, term: term}, (videos) => {
-      this.setState({selectedVideo: videos[0]})
-      this.setState({videos})
-    }) 
-  }
-
-
-  render() {
-
-    const SearchYT = _.debounce( term => {this.SearchYT(term)}, 300);
+  render(){
     return (
-      <div>
-        <SearchBar onNewSearchTerm={ (term) => this.SearchYT(term) } />
-        <div className="row">
-          <VideoDetail selectedVideo={this.state.selectedVideo} />
-          <VideoList
-            videos={this.state.videos}
-            setVideo={ (video) => this.setState({selectedVideo: video}) } />
-          </div>
-      </div>
+      <div>Hello</div>
     )
   }
-
 }
+
 
 ReactDOM.render(<App />, document.querySelector('.container'));
