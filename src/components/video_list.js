afaf7ff99c06e487be36ffe33265141363cc9f60
jhyun94 <hyunjonathan@gmail.com>
2018-02-28 16:49:33 -0600

    video list items

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-	console.log(props.videos);
 	const videos = props.videos.map( (video) => {
-		console.log(video);
-		return <VideoListItem key={video.id.videoId}/>
+		return <VideoListItem 
+			key={video.id.videoId}
+			video={video} />
 	})
 	return (
 		<ul className="col-md-4 list-group">
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
-const VideoListItem = (props) => {
-
+const VideoListItem = ({video}) => {
+	const imageUrl = video.snippet.thumbnails.default.url;
+	const title = video.snippet.title;
 	return (
-		<div>test</div>
+		<li className="list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" src={imageUrl}/>
+				</div>
+
+				<div className="media-body">
+					<div className="media-heading">
+						{title}
+					</div>
+				</div>
+			</div>
+		</li>
 	)
 }
 
